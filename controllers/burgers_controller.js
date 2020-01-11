cc9fdc4d80ffeaa0e430139d986e81a0866d1479
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2020-01-10 23:20:52 -0600

    Wrote the remaining routes for the burgers_controller.js file - routes include get, post, put and delte routes.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const burger = require("../models/burger");
 
-//Get Router
+//Get, Post, and Delete Router Function
 const router = express.Router();
 
 router.get("/", function(req, res) {
@@ -12,12 +12,49 @@ router.get("/", function(req, res) {
         console.log(hdbrsObj);
         res.render("index", hdbrsObj);
     });
+
+    router.post("/api/burgers", function(req, res){
+        burger.insertOne(
+            ["burger_name", "devoured"],
+            [req.body.burger_name, req.body.devoured],
+            function(result) {
+                res.json({ id: result.insertID });
+            }
+        );
+    });
+
+    router.put("/api/burgers/ :id", function(req, res) {
+        let condition = "id = " + req.params.id;
+        
+        console.log("condition", condition);
+        burger.updateOne({ devoured: req.body.devoured }, condition, function(
+            result
+        ) {
+            if ((result, changedRows === 0)) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
+
+    router.deleteOne(condition, function(req, res){
+        let condition = "id = " + req.params.id;
+        console.log("condition", condition);
+
+        burger.deleteOne(condition, function(result) {
+            if((result, changedRows === 0)) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
 });
 
-//Post Router
+//Export Router
+ 
+module.exports = router;
 
 
-//Delete Router
 
-
-//Put Router
