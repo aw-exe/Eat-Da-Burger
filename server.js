750a80b6e03bb799846bcb8f24e663beb04cd47a
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2020-01-10 21:34:18 -0600

    Added require body parser and express handlebars to my server.js file, confirmed in git bash terminal that app is listening on port 8080.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require ("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
 
 let PORT = process.env.PORT || 8080;
 
 let app = express();
 
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
