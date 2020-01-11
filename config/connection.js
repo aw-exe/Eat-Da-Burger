1fbff1d4134d444a611696e25428f5e379333db6
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2020-01-11 00:43:44 -0600

    I broke my code, not sure how to fix it. Seems to be a naming problem...?

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 8080,
   user: "root",
   password: "",
   database: "burger_db"
--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,29 @@
-const orm = require("../config/orm");
+// let orm = require("../config/orm.js");
 
-//Callback of ORMS
-const burger = {
-    selectAll: function(cb) {
-        orm.selectAll("burgers", function(res) {
-            cb(res);
-        });
-    },
-    insertOne: function(cols, vals, cb) {
-        orm.selectAll("burgers", cols, vals, function(res) {
-            cb(res);
-        });
-    },
-    updateOne: function(objColVals, condition, cb) {
-        orm.selectAll("burgers", objColVals, conditon, function (res) {
-            cb(res);
-        });
-    },
-    deleteOne: function(condition, cb) {
-        orm.selectAll("burgers", condition, function(res) {
-            cb(res);
-        })
-    }
-};
+// const burger = {
+//   selectAll: function(cb) {
+//     orm.selectAll("burgers", function(res) {
+//       cb(res);
+//     });
+//   },
 
-//Exported Variable
-module.exports = burger;
+//   insertOne: function(cols, vals, cb) {
+//     orm.insertOne("burgers", cols, vals, function(res) {
+//       cb(res);
+//     });
+//   },
+
+//   updateOne: function(objColVals, condition, cb) {
+//     orm.updateOne("burgers", objColVals, condition, function(res) {
+//       cb(res);
+//     });
+//   },
+
+//   deleteOne: function(condition, cb) {
+//     orm.deleteOne("burgers", condition, function(res) {
+//       cb(res);
+//     });
+//   }
+// };
+
+// module.exports = burger;
