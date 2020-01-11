2a38d8a78487bd135017af12598ca10ec4761460
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2020-01-10 21:43:23 -0600

    Changed Controllers.js folder to Controllers - didn't realize I had added the javascript filepath to the folder name by accident. Wrote the static routes, required npm packages, template engine connection and port listener for server.js file.

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
 let PORT = process.env.PORT || 8080;
-
 let app = express();
+
+//Static Routes
+
+app.use(express.static("public"));
+
+//Body Parser Element
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Template Engine Connection
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+let routes = require("./controllers/burgers_controller")
+app.use(routes);
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
 });
+
