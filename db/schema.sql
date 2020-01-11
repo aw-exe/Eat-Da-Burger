886e0467769113c73aced7d064ea2ba929ae7f2f
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2020-01-10 21:00:32 -0600

    Created table inside the schema.sql file and created my database inside on mySQL.

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,10 @@
-Write SQL queries this file that do the following:
+CREATE DATABASE burgers_db;
 
-Create the burgers_db.
-Switch to or use the burgers_db.
-Create a burgers table with these fields:
-id: an auto incrementing int that serves as the primary key.
-burger_name: a string.
-devoured: a boolean.
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT, 
+    burger_name VARCHAR (50) NOT NULL, 
+    devoured BOOLEAN DEFAULT false, 
+    PRIMARY KEY (id)
+);
